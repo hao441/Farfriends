6780bf3fdb471e38d063f12a01ee893431d54f15
Harrison <103507184+hao441@users.noreply.github.com>
2023-01-30 09:31:18 +1100

    make it better

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 let express = require('express');
-let cors = require('cors');
 const { trainModel, farFriendResponse } = require('./model');
 
 let app = express();
-const port = 9000;
+const port = process.env.PORT || 9000;
 
 //middleware
-app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.use(express.json());
 
 app.listen(port, () => {
